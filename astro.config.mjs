eac98f1b789b35c41ec4494a48512b0f4f424761
Leiner117 <lealvarado@estudiantec.cr>
2024-02-01 22:16:11 -0600

    try fix error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://leiner117.github.io/minimalist-portfolio/',
-    base: 'https://github.com/Leiner117/minimalist-portfolio'
+    site: 'https://leiner117.github.io/minimalist-portfolio/'
 });
